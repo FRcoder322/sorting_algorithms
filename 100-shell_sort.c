3d321215f5841a01bb058d791385c6bdd0cd905e
Florence Rotich <kuravalley@gmail.com>
2023-07-25 19:47:24 +0300

    function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * swap_ints -  Its Swap two integers in an array.
+ * @a: its the  first integer to swap.
+ * @b: its the  second integer to swap.
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort -  this is the Sort an array of integers in ascending
+ * @array: Its an array of integers.
+ * @size: its size of the array.
+ *
+ * Description:  its Uses the Knuth interval sequence.
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
